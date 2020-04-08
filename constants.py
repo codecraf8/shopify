497106611cd4f766e01bb07ade83b11c36af7236
codecraf8 <sak3tb@gmail.com>
2020-04-08 11:07:56 +0530

    Update constants.py

--- constants.py
+++ constants.py
@@ -2,4 +2,4 @@ SHOPIFY_API_KEY = '46d1fec9c2e3d96a2d46da99a6d22100'
 SHOPIFY_API_SECRET = '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'
 SHOPIFY_API_VERSION = 'stable'
 SHOPIFY_API_SCOPE = 'read_products,read_orders'
-REDIRECT_URI = "https://9b5fd7f8.ngrok.io/finalize"
+REDIRECT_URI = "https://devapi.karenapp.io/integrations/shopify/finalize"
